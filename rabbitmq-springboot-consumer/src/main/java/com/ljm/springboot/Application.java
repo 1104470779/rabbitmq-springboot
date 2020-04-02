ba38d5bfdf4147026f82a98c9233de2b1eed92a5
ljm <1104470779@qq.com>
2020-04-02 16:41:26 +0800

    springboot整合rabbitmq

--- rabbitmq-springboot-consumer/src/main/java/com/ljm/springboot/Application.java
+++ rabbitmq-springboot-consumer/src/main/java/com/ljm/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.bfxy.springboot;
+package com.ljm.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- rabbitmq-springboot-consumer/src/main/java/com/ljm/springboot/MainConfig.java
+++ rabbitmq-springboot-consumer/src/main/java/com/ljm/springboot/MainConfig.java
@@ -1,10 +1,10 @@
-package com.bfxy.springboot;
+package com.ljm.springboot;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"com.bfxy.springboot.*"})
+@ComponentScan({"com.ljm.springboot.*"})
 public class MainConfig {
 
 }
